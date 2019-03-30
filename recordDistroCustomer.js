b30914d646bfafc13a1e6e140bae160d6b963d3d
hubenschmidt <whubenschmidt@gmail.com>
2019-03-30 01:04:31 -0400

    before saturday class

--- recordDistroCustomer.js
+++ recordDistroCustomer.js
@@ -1,6 +1,6 @@
-//require the mysql database
 
 var mysql = require('mysql');
+var inquirer = require('inquirer');
 
 //connect to the mysql databse with creentials
 
@@ -12,17 +12,73 @@ var connection = mysql.createConnection({
     database: 'recordDistro_db'
 })
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId + '\n');
-    readProducts();
+    start();
+    // readProducts();
 })
 
-//function to read
+//function which prompts the user for ID of product they would like to buy, and how many units they would like to buy
 
-function readProducts(){
+function start() {
+
+    //query the database for all items listed in products table
+    connection.query('SELECT * FROM products',
+    function (err, res){
+        if (err) throw err;
+    
+    inquirer
+        .prompt([
+            {
+                name: 'buyWhat',
+                type: 'list',
+                message: 'Which record would you like to buy?',
+                choices: function(){
+                    var choiceArr = [];
+                    for (var i = 0; i < res.length; i++) {
+                        choiceArr.push(res[i].product_name);
+                    }
+                return choiceArr;
+                }
+            },
+            {
+                name: 'howMany',
+                type: 'input',
+                message: 'How many copies?',
+                validate: function(value){
+                    if (isNaN(value) === false){
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(function(answer){
+            var chosenProduct;
+            for (var i = 0; i < res.length; i++){
+                if (res[i].product_name === answer.choice) {
+                    chosenProduct = res[i];
+                    console.log(answer.choice)
+                }
+            }
+
+
+            //determine if bid was high enough
+
+            if (chosenProdudct)
+
+           
+
+        })
+
+    })
+}
+
+
+function readProducts() {
     console.log('Display all products...\n')
-    connection.query('SELECT * FROM products', function(err, res){
+    connection.query('SELECT * FROM products', function (err, res) {
         if (err) throw err;
         console.log(res);
         connection.end();
