379d8e24207f88ca8ce5b99902a3432cfb66d44f
hubenschmidt <whubenschmidt@gmail.com>
2019-04-07 13:26:27 -0400

    tested edge cases

--- supervisor.js
+++ supervisor.js
@@ -28,21 +28,23 @@ function mainMenu() {
                     name: 'menu',
                     type: 'list',
                     message: "Choose an action..",
-                    choices: ['View product sales by Department', 'Create New Department', 'Exit']
+                    choices: ['View Sales by Department', 'Create Department', 'Exit']
 
                 }
             ]
         ).then(function (answer) {
             switch (answer.menu) {
-                case 'View product sales by Department': viewProductsByDept();
+
+                case 'View Sales by Department': viewProductsByDept();
                     break;
-                case 'Create new Department': createDepartment();
+                case 'Create Department': createDepartment();
                     break;
                 case 'Exit': exitManager();
 
             }
         })
 }
+
 var cellArr
 var t;
 
@@ -60,7 +62,7 @@ function cell(value) {
 function viewProductsByDept() {
 
     connection.query(
-        'SET SESSION sql_mode="STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION";SELECT d.department_id AS dept_id, d.department_name AS dept_name, d.over_head_costs AS overhead_cost, p.department_name, IFNULL(SUM(p.product_sales), 0) AS product_sales, IFNULL(SUM(p.product_sales), 0) - IFNULL(d.over_head_costs, 0) AS total_profit FROM departments AS d LEFT JOIN products AS p ON d.department_name = p.department_name GROUP BY d.department_name ORDER BY total_profit', 
+        'SET SESSION sql_mode="STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION";SELECT d.department_id AS dept_id, d.department_name AS dept_name, IFNULL(d.over_head_costs,0) AS overhead_cost, p.department_name, IFNULL(SUM(p.product_sales), 0) AS product_sales, IFNULL(SUM(p.product_sales), 0) - IFNULL(d.over_head_costs, 0) AS total_profit FROM departments AS d LEFT JOIN products AS p ON d.department_name = p.department_name GROUP BY d.department_name ORDER BY total_profit',
         function (err, res) {
             if (err) throw err;
             var propertyIndexArr = []
@@ -69,12 +71,10 @@ function viewProductsByDept() {
             buildHeaders(firstIndex)
             buildRows(res[1])
 
-          
-
             function buildHeaders(arr) {
                 var headers = ' ' + cell(arr[0]) + ' ' + cell(arr[1]) + ' ' + cell(arr[2]) + ' ' + cell(arr[4]) + ' ' + cell(arr[5]);
-                console.log('\n                                                           Total Profit by Department\n'.green.underline +
-                    headers+ '\n' +
+                console.log('\n                                                           Total Profit by Department\n'.green.underline.bgBlack +
+                    headers.bold.bgBlack + '\n' +
                     '----------------| ---------------| ---------------| ---------------| ---------------|'.grey)
             }
 
@@ -85,10 +85,10 @@ function viewProductsByDept() {
                     var rows = ' ' + cell(items[0]) + ' ' + cell(items[1]) + ' ' + cell(items[2]) + ' ' + cell(items[4]) + ' ' + cell(items[5]);
 
 
-                    var format = rows.white+ '\n' +
-                    '----------------|'.grey+'----------------|'.grey+'----------------|'.grey+'----------------|'.grey+'----------------|'.grey
+                    var format = rows.white + '\n' +
+                        '----------------|'.grey + '----------------|'.grey + '----------------|'.grey + '----------------|'.grey + '----------------|'.grey
                     console.log(format)
-                    
+
                 })
             }
             exitView()
@@ -97,54 +97,46 @@ function viewProductsByDept() {
 }
 
 function createDepartment() {
+    connection.query('SELECT * FROM products',
+        function (err, res) {
+            if (err) throw err;
 
-    console.log('hellow')
+            inquirer
+                .prompt(
+                    [
+                        {
+                            name: 'department_name',
+                            type: 'input',
+                            message: 'Department name?',
+                            validate: function (value) {
+                                if (typeof value === 'string' || value instanceof String) {
+                                    return true
+                                }
+                                return false
+                            }
+                        },
+                    ]).then(function (answer) {
+                        var query = connection.query('INSERT INTO departments SET ?',
+                            {
+                                department_name: answer.department_name
+                            },
+                            function (err, res) {
 
-    connection.query('SELECT * FROM departments', 
-    function(err, res){
-        if (err) throw err;
+                                if (err) throw err;
 
-        console.log(connection.sql)
+                                console.log('\n\n' + answer.department_name.green.bold + ' added to the department list...' +
+                                    '\nThe following change was made:'.bgGreen.black)
 
-        // inquirer
-        // .prompt(
-        //     [
-        //         {
-        //             name: 'department_name',
-        //             type: 'input',
-        //             message: 'Department name?',
-        //             validate: function (value) {
-        //                 if (typeof value === 'string' || value instanceof String) {
-        //                     return true
-        //                 }
-        //                 return false
-        //             }
-        //         },
-        //     ]).then(function(answer){
+                                console.log(query.sql.yellow + '\n\n')
+                                console.log('____________________________________\n'.underline.yellow)
+                                exitView();
+                            }
 
-        //         console.log(answer.department_name)
+                        )
 
-        //         var query = connection.query('INSERT INTO departments SET ?',
-        //         {
-        //             department_name: answer.department_name
-        //         },
-        //         function (err, res){
-
-        //             if (err) throw err;
-
-        //             console.log('\n\n' + answer.department_name + ' added to the department list...' +
-        //             '\n The following change was made:'.bgGreen.black)
-
-        //                     console.log(query.sql.yellow + '\n\n')
-        //                     console.log('____________________________________\n'.underline.yellow)
-        //                     exitView();
-        //         }
-                 
-        //         )
-        
-        //     })
-    })
-        }
+                    })
+        })
+}
 
 function exitView() {
     inquirer
